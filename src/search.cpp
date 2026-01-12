38570f6bfe6161f80a0b7698c47125c350123c84
Jakub Ciolek <jakub@ciolek.dev>
2026-01-12 18:34:22 +0100

    qsearch: move moveCount gate lower

--- src/search.cpp
+++ src/search.cpp
@@ -1635,8 +1635,6 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
             if (!givesCheck && move.to_sq() != prevSq && !is_loss(futilityBase)
                 && move.type_of() != PROMOTION)
             {
-                if (moveCount > 2)
-                    continue;
 
                 Value futilityValue = futilityBase + PieceValue[pos.piece_on(move.to_sq())];
 
@@ -1648,6 +1646,8 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
                     continue;
                 }
 
+                if (moveCount > 2)
+                    continue;
                 // If static exchange evaluation is low enough
                 // we can prune this move.
                 if (!pos.see_ge(move, alpha - futilityBase))
