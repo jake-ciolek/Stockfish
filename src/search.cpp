a4581fd06d0d163dbe6dbfc46ebeffa97bf5c1c4
Jakub Ciolek <jakub@ciolek.dev>
2026-01-22 23:11:29 +0100

    try 3

--- src/search.cpp
+++ src/search.cpp
@@ -1879,8 +1879,8 @@ void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus) {
 
     for (const auto [i, weight] : conthist_bonuses)
     {
-        // Only update the first 2 continuation histories if we are in check
-        if (ss->inCheck && i > 2)
+        // Only update the first 3 continuation histories if we are in check
+        if (ss->inCheck && i > 3)
             break;
 
         if (((ss - i)->currentMove).is_ok())
