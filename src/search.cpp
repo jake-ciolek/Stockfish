29ada90a8aefa06a22e303867fa5d4729599d7cd
Jakub Ciolek <jakub@ciolek.dev>
2026-01-23 18:03:19 +0100

    qsearch: try a pawn bonus

--- src/search.cpp
+++ src/search.cpp
@@ -1638,7 +1638,10 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
                 if (moveCount > 2)
                     continue;
 
-                Value futilityValue = futilityBase + PieceValue[pos.piece_on(move.to_sq())];
+                Piece capturedPiece = pos.piece_on(move.to_sq());
+
+                Value futilityValue =
+                  futilityBase + PieceValue[capturedPiece] + (type_of(capturedPiece) == PAWN) * 64;
 
                 // If static eval + value of piece we are going to capture is
                 // much lower than alpha, we can prune this move.
