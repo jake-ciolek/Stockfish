1f558f497034453a31a7f3b7b4a5704b6a9ea546
Jakub Ciolek <jakub@ciolek.dev>
2026-02-05 16:36:26 +0100

    mv3 + 50

--- src/search.cpp
+++ src/search.cpp
@@ -1636,7 +1636,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
             if (!givesCheck && move.to_sq() != prevSq && !is_loss(futilityBase)
                 && move.type_of() != PROMOTION)
             {
-                if (moveCount > 2)
+                if (moveCount > 3)
                     continue;
 
                 Value futilityValue = futilityBase + PieceValue[pos.piece_on(move.to_sq())];
@@ -1651,7 +1651,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
 
                 // If static exchange evaluation is low enough
                 // we can prune this move.
-                if (!pos.see_ge(move, alpha - futilityBase))
+                if (!pos.see_ge(move, alpha - futilityBase + 50 * (moveCount == 3)))
                 {
                     bestValue = std::max(bestValue, std::min(alpha, futilityBase));
                     continue;
