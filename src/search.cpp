4ef28860f8f1131bb7bb5b1a3aa9cdc71e77d8c6
Jakub Ciolek <jakub@ciolek.dev>
2026-02-12 09:24:55 +0100

    pv * 80

--- src/search.cpp
+++ src/search.cpp
@@ -1073,7 +1073,7 @@ moves_loop:  // When in check, search starts here
 
                 // SEE based pruning for captures and checks
                 // Avoid pruning sacrifices of our last piece for stalemate
-                int margin = std::max(166 * depth + captHist / 29, 0);
+                int margin = std::max(166 * depth + captHist / 29, PvNode * 80);
                 if ((alpha >= VALUE_DRAW || pos.non_pawn_material(us) != PieceValue[movedPiece])
                     && !pos.see_ge(move, -margin))
                     continue;
